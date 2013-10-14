f06e52d4e6f2890c6d22c2746e053eca2ea36a3c
Marc Schoolderman <mschool@science.ru.nl>
2013-10-14 18:05:35 +0200

    [usability improvement] gcc.log now contains results of all .cpp files; do not generate empty logs

--- trialc.sh
+++ trialc.sh
@@ -10,8 +10,11 @@ CXX="g++ -std=c++0x -fsyntax-only -Wall -Wextra -pedantic"
 MYDIR="${0%/*}"
 
 for dir in "$@"; do
-	for f in `ls "$dir"/*.[Cc] "$dir"/*.[Cc][Pp][Pp] 2> /dev/null`; do
-		$CXX "${f}" 2> "$dir"/gcc.log
-	done
+	for f in "$dir"/*.[Cc] "$dir"/*.[Cc][Pp][Pp]; do
+		test -e "$f" && $CXX "${f}"
+	done 2> "$dir"/gcc.log
 	"$MYDIR/jarify.sh" "$dir" 2> "$dir"/java.log
+	for log in "$dir"/gcc.log "$dir"/java.log; do
+		test -s "$log" || rm -f "$log"
+	done
 done
