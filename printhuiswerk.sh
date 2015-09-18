f52b900b36676def5136bb3b8343af4295a4ceb5
Marc Schoolderman <mschool@science.ru.nl>
2015-09-18 17:52:34 +0200

    tweaks

--- printhuiswerk.sh
+++ printhuiswerk.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 PRINTER=${1:-lazarus}
-OPTIONS="-o PaperSources=PC210 -o Finisher=FS533 -o InputSlot=AutoSelect -o OutputBin=Default -o Binding=LeftBinding -o KMDuplex=2Sided -o SelectColor=Grayscale -o PageSize=A4 -o PageRegion=A4 -o Staple=1Staple(Left)"
+OPTIONS="-o PaperSources=PC210 -o Finisher=FS533 -o InputSlot=AutoSelect -o OutputBin=Default -o Binding=LeftBinding -o KMDuplex=2Sided -o SelectColor=Grayscale -o PageSize=A4 -o PageRegion=A4 -o Staple=1Staple(Left) -o Duplex=DuplexNoTumble -o OptionDuplex=True -o HPOption_Duplexer=True"
 
 STUDDIRS="[sez][0-9][0-9]*"
 
@@ -9,10 +9,10 @@ lpq -P${PRINTER} | grep 'unknown' && exit 1
 set -e
 
 for pdf in ${STUDDIRS}/*.[pP][dD][fF]; do 
-  echo "$pdf"
   studnr="${pdf%/*}"
   if [ ! -r "${pdf}.printed" ];
   then
+    echo "$pdf"
     pdftops "$pdf" - | lpr -J "$studnr" -P${PRINTER} ${OPTIONS}
     touch "${pdf}.printed"
     sleep 3s
