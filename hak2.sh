44c4343c33d6edc0b3ed95ea4ffb55f1f92bacdc
Marc Schoolderman <mschool@science.ru.nl>
2015-10-02 18:41:13 +0200

    already-present directories attract (newer) contents

--- hak2.sh
+++ hak2.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 # verdeel alle s* directories in gelijke delen over de argumenten
+# als de argumenten al bestaan en mapjes bevatten, kopieert het de inhoud
 
 if [ -z "$1" ]; then
     echo "usage: hak2.sh <dir1> <dir2> ... <dirN>"
@@ -12,6 +13,14 @@ N=$#
 i=$RANDOM
 until [ -z "$1" ]; do
    mkdir -p "$1"
+   # move contents of directories already present
+   for prestud in "$1"/[sez][0-9]*; do
+       stud="${prestud##*/}"
+       if [ -d "$prestud" ] && [ -d "$stud" ]; then
+	   mv -u "$stud"/* "$prestud"
+	   rm -rd "$stud"
+       fi
+   done
    dir[$((i++%N))]="$1"
    shift
 done
@@ -19,6 +28,6 @@ done
 # de 'grep' is hier slechts een extra safety; zou eigenlijk niet nodig moeten zijn.
 # als je die aanpast wil je ook in groepjes.sh waarschijnlijk even rondneuzen.
 i=0
-for stud in `ls -d [sez][0-9]* | grep "[sez][0-9]\{6,7\}" | sort -R`; do
+for stud in `ls -d [sez][0-9]* 2> /dev/null | grep "[sez][0-9]\{6,7\}" | sort -R`; do
    mv "$stud" "${dir[$((i++%N))]}"
 done
