a6d50858ae74f705469bc5daa7a7dace4ae0834d
Lucus16 <lars.jellema@gmail.com>
2015-11-26 18:12:36 +0100

    Cleaned up mksdir.sh
    You can now pass a full name and it will use the last word in the name to find
    the corresponding email address and student number. This works because all
    student.ru.nl email addresses are required to end in the last name. If it
    doesn't work just enter something that does match and change the name manually.

--- mksdir.sh
+++ mksdir.sh
@@ -3,40 +3,35 @@
 # create a emulated s123456 directory + fake response file
 # use if someone who submitted manually :(
 
+# Usage: $ ./mksdir.sh Full Name
+
 set -e
 
-NAME="$1"
+FULLNAME="$@"
+NAME="${FULLNAME##* }"
+ASSIGNMENT="Week x"
 
 select name in $(grep -i "$NAME" "${0%/*}/userlist" | tr '\t' '|'); do
 	DIR=`echo "$name" | cut -f1 -d'|'`
 	break
 done
 
-if [ -e "$DIR" ]; then
-	echo "$DIR" already exists!
-	exit 1
-fi
-
 if [ -z "$DIR" ]; then
 	echo User not found.
 	exit 1
 fi
 
-if [ -z "$DIR" ]; then
-	echo Aborted.
+if [ -e "$DIR" ]; then
+	echo "$DIR" already exists!
 	exit 1
 fi
 
 echo "creating $DIR"
 mkdir -p "$DIR"
 cat >> "$DIR/$DIR.txt" <<EOF
-Name: $NAME ($DIR)
-Assignment:Week x
-Date Submitted:$(date)
-Current Grade:Not Yet Graded
+Name: $FULLNAME ($DIR)
+Assignment: $ASSIGNMENT
+Date Submitted: $(date)
+Current Grade: Not Yet Graded
 
-Submission Field:
-gesubmit per email
-Files:
-$*
 EOF
