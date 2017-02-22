78644b72ff98ac6ce0fdf66ec84cb8786883edf4
Marc Schoolderman <squell@xs4all.nl>
2017-02-22 20:44:54 +0100

    respect system-wide EDITOR setting

--- pol.sh
+++ pol.sh
@@ -3,12 +3,10 @@
 # pol maakt het geven van uitgebreide feedback aan meerdere
 # mensen gemakkelijker (maar kijk niet hoe)
 
-EDITOR=vi
-
 VAR='^\(\$[[:alnum:][:space:]_]\+\)'
 TEMP=`mktemp`
 sed "/${VAR}:$/",'/^\$$/!d;/^\$/{s/://;/\$$/d}' "$@" >> "$TEMP"
 grep -ho "${VAR}$" "$@" | grep -Fvxf "$TEMP" - | sort -u >> "$TEMP"
-$EDITOR "$TEMP"
+${EDITOR:-vi} "$TEMP"
 sed -i -e "/${VAR}:$/d;/^\$$/s///;$(sed "s:${VAR}$:"'\n/^\1$/c:;s/\\/\\\\/g;s/$/\\/;$s/$/\n\n#/' "$TEMP")" "$@"
 rm -f "$TEMP"
