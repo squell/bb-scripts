6a30747b80f3a8fece67a5b27be2c037caf01f66
Marc Schoolderman <squell@xs4all.nl>
2018-09-07 00:47:09 +0200

    replaced HTML2Text with something homegrown, so we can actually see what students are up to

--- bsunzip.sh
+++ bsunzip.sh
@@ -30,6 +30,18 @@ unzip -q -o -d "$DEST" "$1"
 
 ADDED="#comments.txt"
 
+strip_cruft() {
+	# 1) remove all line feeds
+	# 2) filter out all inner table cells; <td>...</td>, where "..." doesn't contain opening/closing tags starting with a t
+	# 3) keep only every 2nd line (which contains the actual comment)
+	# 4) eliminate the brightspace HTML boilerplate
+	# 5) replace <p></p>, &amp, and <br>
+	tr -d '\r\n' | egrep -o '<td[^>]*>([^<]*(<([^/t]|/[^t])[^>]*>)?)*</td>' | sed -n 'n;p;n' |
+		sed "s|<td width=80% valign=top>\(.*\)<p style='margin-top:3px;'><b>Comments:</b><br>\(.*\)</td>|\2|" |
+		tr -d '\n' | sed 's|<br />|\n|g; s|<p>\([^<]*\)</p>|\1\n|g; s|&amp;|\&|g'
+
+}
+
 getcomment() {
 	tr -d '\n' < "$DEST"/index.html | sed 's/<tr bgcolor=#AAAAAA>/\n&/g' | grep -F "$1" | grep -F "$2"
 }
@@ -40,12 +52,16 @@ for submission in "$DEST"/*/; do
 	surname="${date% -*}"
 	surname="${surname##* }"
         date="${date##*- }"
-	if getcomment "$date" "$surname" | grep -F -e '<script'  -e '<style'; then
+	comment="$(getcomment "$date" "$surname")"
+	if echo "$comment" | grep -F -e '<script'  -e '<style'; then
 		echo "$submission: contains suspicious tags!"
-		echo "We need to talk:" >  "$submission/NOT_APPRECIATED.TXT"
-		getcomment "$date" "$surname" >> "$submission/NOT_APPRECIATED.TXT"
+		echo "Report security problems and instead try to exploit them. Your attempt has been flagged." >  "$submission/WARNING.TXT"
 	fi
-	getcomment "$date" "$surname" | html2text -nobs | sed '1d' > "${submission}${ADDED}"
-	touch -r "$DEST"/index.html "${submission}${ADDED}"
+	echo -n "$comment" | strip_cruft > "${submission}${ADDED}"
+	if [ -s "${submission}${ADDED}" ]; then
+		touch -r "$DEST"/index.html "${submission}${ADDED}"
+	else
+		rm -f "${submission}${ADDED}"
+	fi
 done
 rm -f "$DEST"/index.html
