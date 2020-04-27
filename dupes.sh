ae0391127755a5e88d9a301b6016fae5604db223
Markus Klinik <mkl@lambdanaut.net>
2020-04-27 11:35:33 +0200

    more verbose variable and function names

--- dupes.sh
+++ dupes.sh
@@ -12,19 +12,19 @@ cvt() {
     tr -c '[:print:][:cntrl:]' '?' | col
 }
 
-remcom() {
+remove_comments() {
     tr -cd '[:print:][:cntrl:]' | sed -rn ':0 N;${s:/\*([^*]|\*[^/])*\*/: :g;p};b0' | sed 's://.*$::'
 }
 
-strk() {
+collapse_string_constants() {
     #sed 's/""//g;s/"[^"]\+"/"/g'
     #sed 's/"[^"]*"//g'
     sed "s/'.'/'/g"';s/"\(\\.\|[^"]\)*"/"/g'
 }
 
-filter() {
+fingerprint() {
     echo -n 0
-    cat "$1" | remcom | strk | sed 's/[><=]=/<>/g;s/</>/g;s/\(package\|import\) [A-Za-z0-9_.]\+;//g;s/return/!!/g;s/public/$/g;s/class/#/g;s/private/$/g;s/final//g;s/\(if\|switch\)/?/g;s/\(for\|while\)/?/g;s/\<[A-Z][A-Za-z0-9_]*\>/I/g;s/\<[a-z][A-Za-z0-9_]\+\>/i/g;s/[0-9]\+/i/g' | tr -cd '5!?iI#<>\n:~*$\\[]{}()"' | tr ' \t\n\\"' 'abcde' | sed -r 's/c+/c/g;s/c$//'
+    cat "$1" | remove_comments | collapse_string_constants | sed 's/[><=]=/<>/g;s/</>/g;s/\(package\|import\) [A-Za-z0-9_.]\+;//g;s/return/!!/g;s/public/$/g;s/class/#/g;s/private/$/g;s/final//g;s/\(if\|switch\)/?/g;s/\(for\|while\)/?/g;s/\<[A-Z][A-Za-z0-9_]*\>/I/g;s/\<[a-z][A-Za-z0-9_]\+\>/i/g;s/[0-9]\+/i/g' | tr -cd '5!?iI#<>\n:~*$\\[]{}()"' | tr ' \t\n\\"' 'abcde' | sed -r 's/c+/c/g;s/c$//'
 }
 
 if [ -z "$*" ]; then
@@ -33,11 +33,11 @@ if [ -z "$*" ]; then
 fi
 
 if [ ! -d "$1" ]; then
-        filter "$1"
+        fingerprint "$1"
         exit
 fi
 
-declare -A sig
+declare -A fingerprints
 
 # enable recursive globbing **/*.java
 shopt -s globstar
@@ -48,11 +48,11 @@ for arg in "$@"; do
     for file in "$arg"/**/*.java; do
         test -e "$file" || break
         test -e "${file}.SUSPECT" && continue
-        code=`filter "$file"`
+        code=`fingerprint "$file"`
         if [ "${#code}" -ge 42 ]; then
-            found="${sig[$code]}"
+            found="${fingerprints[$code]}"
             if [ -z "$found" ]; then
-                sig[$code]="$file"
+                fingerprints[$code]="$file"
             elif [ "${found%%/*}" = "${file%%/*}" ]; then
                 # not a duplicate, because it is from the same hand-in
                 true
